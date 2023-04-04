e3cad6e343eefcdf82320eb400d0cceabece6deb
Dmitry Rukovichnikov <drukavichnikau@exadel.com>
2023-04-04 12:03:11 +0400

    feat: add board component with bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainViewComponent } from './pages/main-view/main-view.component';
+import { BarComponent } from './components/bar/bar.component';
+import { BoardComponent } from './components/board/board.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainViewComponent
+    MainViewComponent,
+    BarComponent,
+    BoardComponent
   ],
   imports: [
     BrowserModule,
